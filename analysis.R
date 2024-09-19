d1f0a8f0e9420dec9931a8ac35947f3cf5fa492b
Gaurangi <166051396+gr98765@users.noreply.github.com>
2024-09-18 21:29:57 -0700

    Updated clean data by region.R

--- analysis.R
+++ analysis.R
@@ -1,12 +1,19 @@
-
-
-
 # Global Terrorism Database Collaborative Analysis
 
-# Calculate total fatalities per region
-fatalities_per_region <- cleaned_data %>%
-  group_by(country_txt) %>%
-  summarise(total_fatalities = sum(nkill))
+# Load necessary libraries
+library(dplyr)
+library(ggplot2)
 
-# View the results
-glimpse(fatalities_per_region)
+#Role 1: Gaurangi
+# Load the dataset
+gtd_data <- read.csv("C:/Users/gaur/INFO-536/globalterrorismdb_0718dist.csv", stringsAsFactors = FALSE)
+
+
+#select 'country_txt' and 'n_kill' columns, drops rows which consist NA
+
+cleaned_data <- gtd_data %>%
+  select(country_txt, nkill) %>%
+na.omit()
+
+# Data exploration by region
+glimpse(cleaned_data)
