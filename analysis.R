33958241e8d97b03fea99128738111e9a5a2d2f6
Devashree Pawar <devashreepawar@arizona.edu>
2024-09-19 01:04:25 -0700

    Fatalities by region updated

--- analysis.R
+++ analysis.R
@@ -1,33 +1,3 @@
-# Global Terrorism Database Collaborative Analysis
-
-# Load necessary libraries
-library(dplyr)
-library(ggplot2)
-
-#Role 1: Gaurangi
-# Load the dataset
-gtd_data <- read.csv("D:/UArizona/INFO_536/INFO_536_CollaborativeExercise/globalterrorismdb_0718dist.csv", stringsAsFactors = FALSE)
-
-
-#select 'country_txt' and 'n_kill' columns, drops rows which consist NA
-
-cleaned_data <- gtd_data %>%
-  select(country_txt, nkill) %>%
-  na.omit()
-
-# Data exploration by region
-glimpse(cleaned_data)
-
-#Role 2 : Sreeharsha
-# Group by region (or country) and calculate total fatalities
-fatalities_per_region <- cleaned_data %>%
-  group_by(country_txt) %>%
-  summarise(total_fatalities = sum(nkill, na.rm = TRUE))
-
-# View the data for graphing
-glimpse(fatalities_per_region)
-
-
 # Role 3: Devashree Pawar
 # Visualize fatalities by region using a bar chart.
 
